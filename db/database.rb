94876224e84df2464dd9787e6cfc3b8baf281998
Justin Woodbridge <jwoodbridge@me.com>
2012-03-28 11:19:37 -0400

    log our sql queries

--- db/database.rb
+++ db/database.rb
@@ -1,12 +1,16 @@
 require 'active_record'
 require 'uri'
 require 'sqlite3'
+require 'logger'
 
 require 'carrierwave'
 require 'carrierwave/orm/activerecord'
+
 
 ActiveRecord::Base.establish_connection(
   :adapter  => 'sqlite3',
   :database => 'database.db',
   :encoding => 'utf8'
 )
+
+ActiveRecord::Base.logger = Logger.new(STDOUT)
