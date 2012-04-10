15461fe67117187f4914bc56a245aad6cf6fd9ea
Justin Woodbridge <jwoodbridge@me.com>
2012-04-09 20:40:51 -0400

    write person create method

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,10 +1,22 @@
 class PeopleController < ApplicationController
   before_filter :authenticate!
+  respond_to :html, :json
 
   def index
     @people = current_user.people
   end
 
   def new
+  end
+
+  def create
+    @person = Person.new params[:person]
+    @person.user_id = params[:user_id]
+
+    flash[:notice] = "Created #{@person.name}" if @person.save
+
+    respond_with [current_user, @person] do |format|
+      format.html { redirect_to :back }
+    end
   end
 end
